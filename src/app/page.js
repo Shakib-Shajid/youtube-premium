40eeec125fb9f6b3d9de938a87dbb2d8c70d7ff4
shakib-shajid <shakibalshajid1682@gmail.com>
2024-10-24 22:48:47 +0600

    price section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Navbar from "../app/components/Navbar"
 import Banner from "./components/Banner";
+import Offer from "./components/Offer";
+import Price from "./components/Price";
 export default function Home() {
   return (
     <>
       <Navbar />
       <Banner />
+      <Offer />
+      <Price />
     </>
   );
 }
